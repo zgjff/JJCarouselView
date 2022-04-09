d70a1612ca647afe4217ecae7cea1e237da4f5f3
zgj <zguijie1005@163.com>
2022-04-09 08:44:48 +0800

    修改pod 文件+spm文件

--- Package.swift
+++ Package.swift
@@ -19,8 +19,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "JJCarouselView",
-            dependencies: []),
+        .target(name: "JJCarouselView", path: "Sources")
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
